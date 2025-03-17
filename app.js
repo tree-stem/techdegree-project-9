88367e9c658e88c1eb83253d8e1b6c6d92068459
Tristan Ringer <rin9er@gmail.com>
2025-03-17 12:24:25 -0700

    modified to test database authentication

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 // load modules
 const express = require('express');
 const morgan = require('morgan');
+const sequelize = require('./models').sequelize; 
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
@@ -41,6 +42,16 @@ app.use((err, req, res, next) => {
 
 // set our port
 app.set('port', process.env.PORT || 5000);
+
+// Test the database connection.
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
 
 // start listening on our port
 const server = app.listen(app.get('port'), () => {
