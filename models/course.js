ac4d29d5b9721edfe1fc6e1d54638bbe788be9f2
Tristan Ringer <rin9er@gmail.com>
2025-03-19 17:03:33 -0700

    modified course.js to validate user input

--- models/course.js
+++ models/course.js
@@ -5,8 +5,30 @@ const {
 module.exports = (sequelize, DataTypes) => {
   class Course extends Model { }
   Course.init({
-    title: DataTypes.STRING,
-    description: DataTypes.TEXT,
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A title is required.',
+        },
+        notEmpty: {
+          msg: 'Please provide a value for "title".',
+        },
+      },
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A description is required.',
+        },
+        notEmpty: {
+          msg: 'Please provide a value for "description".',
+        },
+      },
+    },
     estimatedTime: DataTypes.STRING,
     materialsNeeded: DataTypes.STRING
   }, { sequelize });
