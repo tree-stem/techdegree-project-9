5ca711ac9dc6a3716000f423dbb4bfbac03d1d25
Tristan Ringer <rin9er@gmail.com>
2025-03-19 17:00:27 -0700

    modified user.js to validate user input

--- models/user.js
+++ models/user.js
@@ -5,10 +5,61 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model { }
   User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    emailAddress: DataTypes.STRING,
-    password: DataTypes.STRING
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A firstName is required.',
+        },
+        notEmpty: {
+          msg: 'Please provide a value for "firstName".',
+        },
+      },
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A lastName is required.',
+        },
+        notEmpty: {
+          msg: 'Please provide a value for "lastName".'
+        },
+      },
+    },
+    emailAddress: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        msg: 'Sorry! The email address you provided is already in use.',
+      },
+      validate: {
+        notNull: {
+          msg: 'An email is required.',
+        },
+        isEmail: {
+          msg: 'Please provide a value for "emailAddress".',
+        },
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A password is required.'
+        },
+        notEmpty: {
+          msg: 'Please provide a value for "password".',
+        },
+        len: {
+          args: [8, 20],
+          msg: 'Password must be between 8 and 20 characters in length.',
+        },
+      },
+    },
   }, { sequelize });
 
   User.associate = (models) => {
